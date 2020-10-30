1423359a596c29ad3326ddc7f4c7f87514704fae
Markus <mvondendriesch@gmail.com>
2020-10-30 20:05:45 +0000

    Revert app header to original

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,17 @@ import Fib from './Fib';
 function App() {
   return (
     <Router>
-      <div>
-        <header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
